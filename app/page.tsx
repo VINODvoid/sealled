3f0a108370b34fe2ce920e9580cba61000aaabd6
VINODvoid <iamlearner.v@gmail.com>
2025-06-27 12:57:39 +0530

    fix : World Map

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,7 @@ import { WorldMap } from "@/components/ui/world-map";
 export default function Home() {
   return (
    <div className="flex flex-col items-center justify-center min-h-screen p-4">
-    <WorldMap dots={
-      
-    }/>
+    <WorldMap/>
    </div>
   );
 }
